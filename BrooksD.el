59580c91acba89b05488ebb4375d8481100945f7
Daniel Brooks <db48x@db48x.net>
2011-05-23 14:43:20 -0700

    add functions and keybindings for deleteing extraneous whitespace

--- BrooksD.el
+++ BrooksD.el
@@ -1,6 +1,19 @@
+;;;; maximize all new windows
 (add-to-list 'load-path (concat dotfiles-dir "/maxframe.el"))
 (require 'maxframe)
 (add-hook 'window-setup-hook 'maximize-frame t)
 
+;;;; start in server mode
 (server-mode)
 
+;;;; handy functions for deleting whitespace
+(defun db48x/delete-ws-backward ()
+  (interactive)
+  (delete-region (point) (save-excursion (skip-syntax-backward " ") (point))))
+
+(defun db48x/delete-ws-forward ()
+  (interactive)
+  (delete-region (point) (save-excursion (skip-syntax-forward " ") (point))))
+
+(global-set-key (kbd "C-c C-d") 'db48x/delete-ws-forward)
+(global-set-key (kbd "C-c M-d") 'db48x/delete-ws-backward)

5e65a44451c11c6ae4afc833e343a9893118aeb0
Daniel Brooks <db48x@db48x.net>
2011-07-28 18:51:02 -0700

    load elnode

--- BrooksD.el
+++ BrooksD.el
@@ -2,5 +2,12 @@
 (require 'maxframe)
 (add-hook 'window-setup-hook 'maximize-frame t)
 
+;;;; elnode server
+(add-to-list 'load-path (concat dotfiles-dir "/elnode"))
+(load-library "elnode")
+
+
+
+;;;; emacs server
 (server-mode)
 

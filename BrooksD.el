54942db11698cfc901cf3b06129613b988ed0c5f
Daniel Brooks <db48x@db48x.net>
2011-08-13 07:25:03 -0700

    new macro for adding jsdoc comment

--- BrooksD.el
+++ BrooksD.el
@@ -44,3 +44,7 @@
   "de-urlencode the region between START and END in current buffer."
   (interactive "r")
   (func-region start end #'url-unhex-string))
+
+(fset 'jsdoc-add-for-this-function
+   (lambda (&optional arg) "very simple macro that grabs the arg list for the function after point and creates a skelton javadoc/jsdoc for it. no error checking. put the point on the first line of the function name, before the arg list." (interactive "p") (kmacro-exec-ring-item (quote ([19 40 13 67108896 19 41 left 134217847 1 47 42 42 44 32 25 return 32 42 47 return up 67108896 up 134217765 44 32 return 17 10 32 42 32 64 112 97 114 97 109 32 return 33 18 47 42 127 92 42 5 32 backspace return 32 42 32] 0 "%d")) arg)))
+(global-set-key [24 11 49] 'jsdoc-add-for-this-function)
